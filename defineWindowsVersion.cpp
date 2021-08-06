8348075eebb334b458223ed2df6754d79ce72738
ValeriyaRyzhikova <rvaleriav@yandex.ru>
2021-08-06 15:12:27 +0700

    add function define version OS

--- defineWindowsVersion.cpp
+++ defineWindowsVersion.cpp
@@ -1,6 +1,67 @@
-﻿#include <iostream>
+﻿#include<iostream>
+#include<string>
+#include<windows.h>
+
+std::string getVersion(OSVERSIONINFO osvi) {
+    std::string result;
+
+    switch (osvi.dwMajorVersion)
+    {
+    case 10:
+        result = "Windows 10";
+        break;
+    case 6:
+        switch (osvi.dwMinorVersion)
+        {
+        case 3:
+            result = "Windows 8.1";
+            break;
+        case 2:
+            result = "Windows 8";
+            break;
+        case 1:
+            result = "Windows 7";
+            break;
+        case 0:
+            result = "Windows Vista";
+            break;
+        }
+        break;
+    case 5:
+        switch (osvi.dwMinorVersion)
+        {
+        case 2:
+            result = "Windows XP Edition";
+            break;
+        case 1:
+            result = "Windows XP";
+            break;
+        case 0:
+            result = "Windows 2000";
+            break;
+        }
+        break;
+    }
+
+    return result;
+}
+
+OSVERSIONINFO getInfoOS()
+{
+    OSVERSIONINFO osvi;
+    BOOL bIsWindowsXPorLater;
+
+    ZeroMemory(&osvi, sizeof(OSVERSIONINFO));
+    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+
+    GetVersionEx(&osvi);
+
+    return osvi;
+}
 
 int main()
 {
+    OSVERSIONINFO osvi = getInfoOS();
+    std::cout << getVersion(osvi);
     return 0;
 }
